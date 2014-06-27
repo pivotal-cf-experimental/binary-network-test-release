807eb209a2f172bdb8ff90cfe89d823ea2937069
Eric Malm <emalm@pivotallabs.com>
2014-06-27 01:29:03 -0700

    Add arguments to client job

--- src/client/client.sh
+++ src/client/client.sh
@@ -4,10 +4,11 @@ set -e -x
 
 SERVER=${1}
 PORT=${2:-8080}
-DURATION=${3:-1}
+TIMEOUT=${3:-2}
+DURATION=${4:-1}
 
 while true; do
-  curl -vvv --connect-timeout 1 http://${SERVER}:${PORT} || true
+  curl -vvv --connect-timeout ${TIMEOUT} http://${SERVER}:${PORT} &
 
   echo "Waiting $DURATION seconds to curl $SERVER:$PORT again"
   sleep ${DURATION}
