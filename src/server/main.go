973b315605e401dfa3758b155335ba25da3e432e
Eric Malm <emalm@pivotallabs.com>
2014-06-27 01:29:32 -0700

    Add port flag and pause to server, config in ctl script

--- src/server/main.go
+++ src/server/main.go
@@ -1,11 +1,25 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
 
+var port = flag.Int("port", 8080, "port for server")
+
 func main() {
-	// Simple static webserver:
-	log.Fatal(http.ListenAndServe(":8080", http.FileServer(http.Dir("/tmp"))))
+	flag.Parse()
+
+	addr := fmt.Sprintf(":%d", *port)
+	log.Printf("Serving on %s\n", addr)
+	log.Fatal(http.ListenAndServe(addr, http.HandlerFunc(helloHandler)))
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	time.Sleep(time.Duration(2) * time.Second)
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("you got served!\n"))
 }
